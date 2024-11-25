5ab3f824ed06bbbf934efd3c198600814406034b
wjdrbgus8167 <wjdrbgus8167@naver.com>
2024-11-25 09:06:33 +0900

    FEAT : 로그인 및 댓글

---
+++ src/main/java/com/ssafy/comment/model/CommentDto.java
@@ -0,0 +1,24 @@
+package com.ssafy.comment.model;
+
+import java.util.List;
+
+import com.ssafy.notice.model.FileInfoDto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class CommentDto {
+    private int commentNo;
+    private String contentId;
+    private String userName;
+    private String email;
+    private String content;
+    private String createdDate;
+    private String updatedDate;
+}
--- src/main/java/com/ssafy/gooleoauth/GoogleOAuthController.java
+++ src/main/java/com/ssafy/gooleoauth/GoogleOAuthController.java
@@ -1,6 +1,8 @@
 package com.ssafy.gooleoauth;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -8,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +19,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.ssafy.member.controller.MemberController;
 import com.ssafy.member.model.MemberDto;
 import com.ssafy.member.model.service.MemberService;
+import com.ssafy.util.JwtUtil;
 
+import io.jsonwebtoken.Claims;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,14 +32,14 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Tag(name = "GoogleOAuth Controller", description = "GoogleOAuth로 계정 이동")
 public class GoogleOAuthController {
-
+	private final JwtUtil jwtUtil;
 	 private final MemberService memberServiceoauth;
 	    private final JavaMailSender mailSenderoauth;
 
-	    public GoogleOAuthController(MemberService memberServiceoauth, JavaMailSender mailSenderoauth) {
+	    public GoogleOAuthController(MemberService memberServiceoauth, JavaMailSender mailSenderoauth,JwtUtil jwtUtil) {
 	        this.memberServiceoauth = memberServiceoauth;
 	        this.mailSenderoauth = mailSenderoauth;
-	        
+	        this.jwtUtil = jwtUtil;
 	    }
 	@Value("${google.auth.url}")
     private String googleAuthUrl;
@@ -76,11 +81,7 @@ public class GoogleOAuthController {
     
     @Operation(summary = "Handle Google OAuth callback", description = "구글로 로그인 후 유저 정보 콜백")
     @GetMapping(value = "/login/oauth_google_check")
-    public ResponseEntity<String> oauth_google_check(HttpServletRequest request,
-                                     @RequestParam(value = "code") String authCode,
-                                     HttpServletResponse response, HttpSession session) throws Exception {
-
-        // Google OAuth 토큰 요청 데이터 구성
+    public ResponseEntity<?> oauth_google_check(@RequestParam(value = "code") String authCode) throws Exception {
         GoogleOAuthRequest googleOAuthRequest = GoogleOAuthRequest.builder()
                 .clientId(googleClientId)
                 .clientSecret(googleClientSecret)
@@ -102,38 +103,58 @@ public class GoogleOAuthController {
         log.info("Token Response: {}", googleLoginResponse.toString());
 
         String googleToken = googleLoginResponse.getId_token();
-        
+
         // Google /tokeninfo 엔드포인트 호출
         String requestUrl = UriComponentsBuilder
                 .fromHttpUrl("https://oauth2.googleapis.com/tokeninfo")
                 .queryParam("id_token", googleToken)
                 .toUriString();
 
-        try {
-            MemberDto resultJson = restTemplate.getForObject(requestUrl, MemberDto.class);
-            log.info("Token Info Response: {}", resultJson);
-            MemberDto memberDto = new MemberDto();
-            
-            memberDto.setSub(resultJson.getSub());
-            memberDto.setEmail(resultJson.getEmail());
-            memberDto.setName(resultJson.getName());
-            memberDto.setPicture(resultJson.getPicture());
-            
-            session.setAttribute("userinfo", memberDto.toString());
-            
-            if(memberServiceoauth.emailCheck(resultJson.getEmail())!=1) {
-            	memberServiceoauth.joinMember(memberDto);
-            }
-            MemberDto membersession = memberServiceoauth.loginMember(memberDto);
-            
-            log.info("myLog-LoginUrl : {}",membersession.toString());
-            session.setAttribute("userinfo", membersession);
-            
-            
-            return ResponseEntity.status(HttpStatus.CREATED).body("로그인 성공");
-        } catch (Exception e) {
-            log.error("Error while calling Token Info API: {}", e.getMessage());
-            throw e;
+        MemberDto resultJson = restTemplate.getForObject(requestUrl, MemberDto.class);
+
+        // 사용자 정보 처리
+        MemberDto memberDto = new MemberDto();
+        memberDto.setSub(resultJson.getSub());
+        memberDto.setEmail(resultJson.getEmail());
+        memberDto.setName(resultJson.getName());
+        memberDto.setPicture(resultJson.getPicture());
+
+        if (memberServiceoauth.emailCheck(resultJson.getEmail()) != 1) {
+            memberServiceoauth.joinMember(memberDto);
         }
+
+        MemberDto membersession = memberServiceoauth.loginMember(memberDto);
+        String role = membersession.getRole();
+        String jwtToken = jwtUtil.generateToken(membersession.getEmail(), role,membersession.getPicture());
+
+        // 클라이언트로 리디렉션
+        String clientRedirectUrl = "http://localhost:5173/oauth-callback"; // Vue.js 클라이언트 URL
+        URI redirectUri = UriComponentsBuilder
+                .fromUriString(clientRedirectUrl)
+                .queryParam("token", jwtToken)
+                .queryParam("name", membersession.getName())
+                .queryParam("email", membersession.getEmail())
+                .queryParam("picture", membersession.getPicture())
+                .build()
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        log.info(redirectUri.toString());
+        headers.setLocation(redirectUri);
+        return new ResponseEntity<>(headers, HttpStatus.FOUND); // 302 Redirect
     }
+
+    @GetMapping("/session/userinfo")
+    public ResponseEntity<?> getSessionUserInfo(@RequestHeader("Authorization") String token) {
+        Claims claims = jwtUtil.validateToken(token.replace("Bearer ", ""));
+        Map<String, String> userInfo = new HashMap<>();
+        userInfo.put("email", claims.getSubject());
+        userInfo.put("picture", claims.get("picture", String.class));
+        userInfo.put("role", claims.get("role", String.class));
+        
+        return ResponseEntity.ok(userInfo);
+    }
+
+
+
 }
